ab1b6ab8c953e5e501a180518951b3b311ab1b79
Anton Fomenko <antonfomenko1234@gmail.com>
2024-10-15 18:37:38 +0300

    define __add__

--- src/Product.py
+++ src/Product.py
@@ -16,6 +16,12 @@ class Product:
     def __str__(self):
         return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
 
+
+    def __add__(self, other):
+        """сложение стоимости всего товара"""
+
+        return self.quantity * self.__price + other.quantity * other.__price
+
     @property
     def price(self):
         """геттер для цены"""
