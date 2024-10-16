20fbc93934edb2aa73aa3a4cdf075037774f6253
Anton Fomenko <antonfomenko1234@gmail.com>
2024-10-16 19:09:42 +0300

    update __add__ func for different classes and add tests for it

--- src/LawnGrass.py
+++ src/LawnGrass.py
@@ -1,11 +1,21 @@
 from src.Product import Product
 
 
-class LownGrass(Product):
+class LawnGrass(Product):
     """Класс для определения продукта (газонная трава)"""
 
     def __init__(self, name, description, price, quantity, country, germination_period, color):
+        """Конструктор для определения травы газонной"""
+
         super().__init__(name, description, price, quantity)
         self.country = country
         self.germination_period = germination_period
         self.color = color
+
+
+    def __add__(self, other):
+        """Метод для сложения стоимости товаров вида трава газонная"""
+
+        if type(other) is LawnGrass:
+            return self.quantity * self.price + other.quantity * other.price
+        raise TypeError
--- tests/test_Smartphone.py
+++ tests/test_Smartphone.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.Smartphone import Smartphone
+from src.LawnGrass import LawnGrass
 
 
 @pytest.fixture
@@ -8,7 +9,23 @@ def some_smartphone():
     return Smartphone("Iphone", "last Iphone", 100000, 10, "max", "16", "1 TB", "Black")
 
 
+@pytest.fixture
+def some_lawn_grass():
+    return LawnGrass("Grass", "Nice Grass", 2, 150, "Russia", "2 months", "Red")
+
+
+@pytest.fixture
+def some_smartphone_1():
+    return Smartphone("Samsung", "last model", 150000, 20, "max", "NOTEXPROGIBRIDTURBONITRO", "15 TB", "Silver")
+
+
 def test_Smartphone(some_smartphone):
     assert some_smartphone.name == "Iphone"
     assert some_smartphone.model == "16"
     assert some_smartphone.color == "Black"
+
+
+def test_add_smartphone(some_smartphone, some_smartphone_1, some_lawn_grass):
+    assert some_smartphone + some_smartphone_1 == 4000000
+    with pytest.raises(TypeError):
+        some_smartphone + some_lawn_grass
